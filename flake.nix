c5dc0a12dcb1907f12a62aa80658d54a0ef3da59
BuildTools <unconfigured@null.spigotmc.org>
2023-04-13 21:13:55 -0700

    flake: pkgsFor

--- flake.nix
+++ flake.nix
@@ -8,8 +8,36 @@
     nixpkgs,
     ...
   }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    inherit (nixpkgs) lib;
+    systems = [
+      # Tier 1
+      "x86_64-linux"
+      # Tier 2
+      "aarch64-linux"
+      # "x86_64-darwin"
+      # Tier 3
+      "armv6l-linux"
+      "armv7l-linux"
+      "i686-linux"
+      "mipsel-linux"
+
+      # Other platforms with sufficient support in stdenv which is not formally
+      # mandated by their platform tier.
+      # "aarch64-darwin"
+      "armv5tel-linux"
+      "powerpc64le-linux"
+      "riscv64-linux"
+
+      # "x86_64-freebsd" is excluded because it is mostly broken
+    ];
+    pkgsFor =
+      builtins.listToAttrs
+      (map (system: {
+          name = system;
+          value = nixpkgs.legacyPackages.${system};
+        })
+        systems);
+    mapSystems = fn: builtins.mapAttrs fn pkgsFor;
   in {
     overlays = {
       default = final: _: {
@@ -39,9 +67,9 @@
       };
     };
 
-    packages.${system} = {
+    packages = mapSystems (pkgs: system: {
       default = self.packages.${system}.slight;
       slight = (self.overlays.default null pkgs).slight;
-    };
+    });
   };
 }
